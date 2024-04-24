3337762ee039a02f927b46748c9057e485332882
awandzi <kwamenafo@gmail.com>
2024-04-24 14:06:16 -0700

    zero

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp
 file { '/tmp/school':
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	contains => 'I love Puppet'
+	mode		=> '0744',
+	owner		=> 'www-data',
+	group		=> 'www-data',
+	contains	=> 'I love Puppet'
 }
 
 
